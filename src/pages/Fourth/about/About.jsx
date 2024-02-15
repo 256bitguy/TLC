8b6ffe2dd39d50e8fc3272c2a7af73c965c4f75c
256bitguy <ashishrajsri2233@gmail.com>
2024-02-16 02:34:46 +0530

    added side profile in about.jsx

--- src/pages/Fourth/about/About.jsx
+++ src/pages/Fourth/about/About.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import Avatar from '@mui/material/Avatar';
+import { Card, Typography } from '@mui/material'
 function About() {
   return (
     <div style={{
-        padding:"100px",
-        margin:"30px",
-        textAlign:"center",
+        display:"flex"
        
     }}>
-        <Typography style={{
+       <div style={{
+        flex:1,
+        flexBasis:"200px"
+       }}> 
+       <Card style={{
+        display:"flex",
+        flexDirection:"column",
+        flexWrap:"wrap"
+       }}>
+       <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg"  sx={{ width: 100, height: 100 ,margin:"10px"}}/>
+       <div>
+        <Typography paragraph={true}>
+            Literary Admin
+        </Typography>
+        <Typography variant="h5" margin={"20px"}> 
+            Vivek raj srivastva
+        </Typography>
+       </div>
+       </Card>
+       </div>
+       <div style={{
+        flex:5,
+        flexBasis:"200px",
+        padding:"30px",
+        margin:"40px"}}>
+       <Typography style={{
              lineHeight:"3rem",
              fontSize:"20px"
         }}>
@@ -40,6 +64,7 @@ As the sun sets on another gathering of our literary enclave, the words linger i
 
 
         </Typography>
+       </div>
     </div>
   )
 }
