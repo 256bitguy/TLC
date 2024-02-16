916b5803cb822defa38fa77cc65791250676f466
256bitguy <ashishrajsri2233@gmail.com>
2024-02-16 18:34:39 +0530

    added the marquee tag which scrolls automatically the tag in the upper direction

--- src/pages/First/News/Cards.jsx
+++ src/pages/First/News/Cards.jsx
@@ -1,36 +1,51 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
 export default function BasicCard() {
   return (
-    <Card sx={{ width:"600px",height:"400px", border:"2px solid black" ,padding:"50px",margin:"50px",borderRadius:"4  0px"}}>
-      <Typography>News</Typography>
-      <CardContent>
-        <Typography >
-          Word of the DayCard: a surface-level container for grouping related components.
-Card Content: the wrapper for the Card content.
-Card Header: an optional wrapper for the Card header.
-Card Media: an optional container for displaying background images and gradient layers behind the Card Content.
-Card Actions: an optional wrapper that groups a set of buttons.
-Card Action Area: an optional wrapper that allows users to interact with the specified area of the Card.
-        </Typography>
-        
+    <Card
+      sx={{
+        width: "600px",
+        height: "400px",
+        border: "2px solid black",
+        padding: "50px",
+        margin: "50px",
+        borderRadius: "40px",
+        overflow: "hidden",
+      }}
+    >
+      <Typography style={{ textAlign: "center" }} variant="h4">
+        News
+      </Typography>
+      <CardContent sx={{ height: '100%', overflow: 'hidden' }}>
+        <marquee direction="up" style={{ display: 'block', height: '100%' }}>
+        <ul style={{
+          fontSize:"20px"
+        }}>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+          <li>sdfdfasdf</li>
+        </ul>
+        </marquee>
       </CardContent>
-       
     </Card>
   );
 }
